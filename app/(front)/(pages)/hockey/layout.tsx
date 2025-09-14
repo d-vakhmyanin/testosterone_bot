7bedde1840e8875470b0ffb52a755d0cee67c584
Daniil Vakhmyanin <d.vakhmyanin@gmail.com>
2025-09-14 23:03:04 +0300

    fix: force-dynamic

--- app/(front)/(pages)/hockey/layout.tsx
+++ app/(front)/(pages)/hockey/layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { getMatches } from '@/app/utils/requests/getMatches';
 
 import { MatchesProvider } from '../../context';
-import { getMatches } from '@/app/utils/requests/getMatches';
+
+export const dynamic = 'force-dynamic';
 
 const HockeyLayout: React.FC<React.PropsWithChildren> = async ({ children }) => {
     const data = await getMatches({ type: 'now' });
